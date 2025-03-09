5dda7850250dc021415b882f02d572e1c2c2310b
Om Prakash <prakash.om.1b@gmail.com>
2025-03-09 22:48:18 +0530

    clinic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
+import LoginPage from './login';
+import PatientRegistration from './patientregistration';
+import DoctorDashboard from './Doctor';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+          <Routes>
+            <Route path='/k' element = {<LoginPage />} />
+            <Route path='/patient-registration' element = {<PatientRegistration />} />
+            <Route path='/' element = {<DoctorDashboard />} />
+          </Routes>
+        </Router>
   );
 }
 
